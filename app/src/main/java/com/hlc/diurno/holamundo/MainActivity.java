784d9afe06118e196ca885cdc203ba6ec2b4a837
sergiosanpol <sergiosanpol@gmail.com>
2017-09-28 09:52:07 +0200

    Aplicación duplicar

--- app/src/main/java/com/hlc/diurno/holamundo/MainActivity.java
+++ app/src/main/java/com/hlc/diurno/holamundo/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textoContador;
+    int contador = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +30,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        textoContador = (TextView) findViewById(R.id.tv_contador);
+        textoContador.setText(contador + "");
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void duplica(View v){
+        contador *= 2;
+        textoContador.setText(contador + "");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
